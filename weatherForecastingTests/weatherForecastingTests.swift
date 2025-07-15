fd8d4e582f3e8840ac7e75a42d535a61aa03885c
niranjan <niranjan52933@gmail.com>
2025-07-15 21:52:59 +0530

    Initial Commit

---
+++ weatherForecastingTests/weatherForecastingTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherForecastingTests.swift
+//  weatherForecastingTests
+//
+//  Created by Niranjan Kumar on 15/07/25.
+//
+
+import Testing
+@testable import weatherForecasting
+
+struct weatherForecastingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
